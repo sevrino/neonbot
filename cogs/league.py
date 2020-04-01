1375948b3b5aa523b5e9170ccb16786575afaffd
sevrino <3mecha@naver.com>
2020-04-02 01:08:53 +0900

    lp 조회 추가

--- cogs/league.py
+++ cogs/league.py
@@ -54,6 +54,7 @@ class league(commands.Cog):
                 data = json.load(json_file)
                 solo_tier = data[0]["tier"]
                 solo_rank = data[0]["rank"]
+                point = data[0]["leaguePoints"]
 
         except KeyError:
             embed=discord.Embed(colour=0xFF0000)
@@ -63,63 +64,63 @@ class league(commands.Cog):
 
         if solo_tier == "IRON":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="IRON" + " " + "%s" % solo_rank, colour=0x515A5A)
+            description="IRON" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0x515A5A)
             embed.set_image(url="https://i.imgur.com/mpUoT5g.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
             
         if solo_tier == "BRONZE":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="BRONZE" + " " + "%s" % solo_rank, colour=0x6E2C00)
+            description="BRONZE" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0x6E2C00)
             embed.set_image(url="https://i.imgur.com/LfyvNkt.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "SILVER":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="SILVER" + " " + "%s" % solo_rank, colour=0xD0D3D4)
+            description="SILVER" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0xD0D3D4)
             embed.set_image(url="https://i.imgur.com/731l30m.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "GOLD":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="GOLD" + " " + "%s" % solo_rank, colour=0xF7DC6F)
+            description="GOLD" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0xF7DC6F)
             embed.set_image(url="https://i.imgur.com/RRpiMqG.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "PLATINUM":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="PLATINUM" + " " + "%s" % solo_rank, colour=0xABEBC6)
+            description="PLATINUM" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0xABEBC6)
             embed.set_image(url="https://i.imgur.com/xOaoBtt.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "DIAMOND":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="DIAMOND" + " " + "%s" % solo_rank, colour=0xBB8FCE)
+            description="DIAMOND" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0xBB8FCE)
             embed.set_image(url="https://i.imgur.com/RNFgHcM.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
         
         if solo_tier == "MASTER":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="MASTER" + " " + "%s" % solo_rank, colour=0x8E44AD)
+            description="MASTER" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0x8E44AD)
             embed.set_image(url="https://i.imgur.com/yAT4oPN.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "GRANDMASTER":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="GRANDMASTER" + " " + "%s" % solo_rank, colour=0xEC7063)
+            description="GRANDMASTER" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0xEC7063)
             embed.set_image(url="https://i.imgur.com/TuMPSw5.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
         if solo_tier == "CHALLENGER":
             embed = discord.Embed(title="%s님의 티어입니다." % sumname, value="%s" % solo_tier + " " + "%s" % solo_rank, 
-            description="CHALLENGER" + " " + "%s" % solo_rank, colour=0x0080FF)
+            description="CHALLENGER" + " " + "%s" % solo_rank + " " + "%slp" % point, colour=0x0080FF)
             embed.set_image(url="https://i.imgur.com/9jfZLTn.png")
             embed.set_footer(text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
