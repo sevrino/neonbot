fbc4aa5b7f2fc19acf80dd52f7cbf95f5f3a3428
sevrino <3mecha@naver.com>
2020-07-05 16:30:37 +0900

    롤 솔랭 자랭 배뀜 오류 해결

--- cogs/league.py
+++ cogs/league.py
@@ -4,9 +4,8 @@ The GNU GENERAL PUBLIC LICENSE
 Copyright (c) 2019-2020 sevrino All rights reserved.
 """
 import discord
-import json
 from discord.ext import commands
-from discord import Permissions
+import json
 import requests as r
 
 
@@ -15,7 +14,6 @@ class league(commands.Cog):
         self.bot = bot
 
     @commands.command(name='솔랭')
-    @commands.guild_only()
     async def lol(self, ctx, *, summonername):
 
         with open('./config/setting.json') as json_file:
@@ -29,33 +27,37 @@ class league(commands.Cog):
             response = r.get(summoner)
             file.write(response.content)
 
-        try:
-            with open("./config/league/solo/id.json", "r", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                id = data["id"]
-                sumname = data["name"]
+        with open("./config/league/solo/id.json", "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+            id = data["id"]
+            sumname = data["name"]
 
-        except:
+        tier = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + \
+            id + "?api_key=" + key
+
+        try:
+            with open("./config/league/solo/info.json", "wb") as file:
+                response = r.get(tier)
+                file.write(response.content)
+
+            with open("./config/league/solo/info.json", "r", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+                queue_type = data[0]["queueType"]
+                if queue_type == "RANKED_FLEX_SR":
+                    solo_tier = data[1]["tier"]
+                    solo_rank = data[1]["rank"]
+                    point = data[1]["leaguePoints"]
+                elif queue_type == "RANKED_SOLO_5x5":
+                    solo_tier = data[0]["tier"]
+                    solo_tier = data[0]["rank"]
+                    point = data[0]["leaguePoints"]
+        except UnboundLocalError:
             embed = discord.Embed(colour=0xFF0000)
             embed.add_field(name="오류가 발생했습니다.",
                             value="LOL API 파싱중 오류가 발생했습니다. 개발서버에 문의해주세요.")
             embed.set_footer(
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
-
-        tier = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + \
-            id + "?api_key=" + key
-
-        with open("./config/league/solo/info.json", "wb") as file:
-            response = r.get(tier)
-            file.write(response.content)
-
-        try:
-            with open("./config/league/solo/info.json", "r", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                solo_tier = data[0]["tier"]
-                solo_rank = data[0]["rank"]
-                point = data[0]["leaguePoints"]
 
         except KeyError:
             embed = discord.Embed(colour=0xFF0000)
@@ -65,10 +67,10 @@ class league(commands.Cog):
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
-        except:
+        except IndexError:
             embed = discord.Embed(colour=0xFF0000)
             embed.add_field(name="오류가 발생했습니다.",
-                            value="LOL API 파싱중 오류가 발생했습니다. 개발서버에 문의해주세요.")
+                            value="해당 플레이어는 현재 티어 미배치, 자체 오류 등으로 인해 솔로 랭크 티어가 표기되지 않습니다. 나중에 시도해 주세요.")
             embed.set_footer(
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
@@ -146,7 +148,6 @@ class league(commands.Cog):
             await ctx.send(embed=embed)
 
     @commands.command(name='자랭')
-    @commands.guild_only()
     async def teamrank(self, ctx, *, name):
         with open('./config/setting.json') as json_file:
             json_data = json.load(json_file)
@@ -159,33 +160,37 @@ class league(commands.Cog):
             response = r.get(summoner)
             file.write(response.content)
 
-        try:
-            with open("./config/league/flex/id.json", "r", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                id = data["id"]
-                sumname = data["name"]
+        with open("./config/league/flex/id.json", "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+            id = data["id"]
+            sumname = data["name"]
 
-        except:
+        tier = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + \
+            id + "?api_key=" + key
+        try:
+            with open("./config/league/flex/info.json", "wb") as file:
+                response = r.get(tier)
+                file.write(response.content)
+
+            with open("./config/league/flex/info.json", "r", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+                queue_type = data[1]["queueType"]
+                if queue_type == "RANKED_FLEX_SR":
+                    flex_tier = data[1]["tier"]
+                    flex_rank = data[1]["rank"]
+                    point = data[1]["leaguePoints"]
+                elif queue_type == "RANKED_SOLO_5x5":
+                    flex_tier = data[0]["tier"]
+                    flex_rank = data[0]["rank"]
+                    point = data[0]["leaguePoints"]
+
+        except UnboundLocalError:
             embed = discord.Embed(colour=0xFF0000)
             embed.add_field(name="오류가 발생했습니다.",
                             value="LOL API 파싱중 오류가 발생했습니다. 개발서버에 문의해주세요.")
             embed.set_footer(
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
-
-        tier = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + \
-            id + "?api_key=" + key
-
-        with open("./config/league/flex/info.json", "wb") as file:
-            response = r.get(tier)
-            file.write(response.content)
-
-        try:
-            with open("./config/league/flex/info.json", "r", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                flex_tier = data[1]["tier"]
-                flex_rank = data[1]["rank"]
-                point = data[1]["leaguePoints"]
 
         except KeyError:
             embed = discord.Embed(colour=0xFF0000)
@@ -195,10 +200,10 @@ class league(commands.Cog):
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
 
-        except:
+        except IndexError:
             embed = discord.Embed(colour=0xFF0000)
             embed.add_field(name="오류가 발생했습니다.",
-                            value="LOL API 파싱중 오류가 발생했습니다. 개발서버에 문의해주세요.")
+                            value="해당 플레이어는 현재 티어 미배치, 자체 오류 등으로 인해 자유 랭크 티어가 표기되지 않습니다. 나중에 시도해 주세요.")
             embed.set_footer(
                 text="Copyright (c) 2019-2020 sevrino All rights reserved.")
             await ctx.send(embed=embed)
